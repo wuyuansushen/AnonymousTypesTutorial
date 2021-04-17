83105bef7c1888dea360e6636f35e3fabe35ad83
wuyuansushen <819070864@qq.com>
2021-04-17 20:42:40 +0800

    Add project files.

---
+++ AnonymousTypesTutorial/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AnonymousTypesTutorial
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public int Age { get; set; }
+
+        public Gender gender { get; set; }
+
+        public enum Gender
+        {
+            male=1,
+            female=0
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = new List<Person>() { };
+            source.Add(new Person() { Name="A",Height=1.1,Weight=74.2,Age=12,gender=Person.Gender.male});
+            source.Add(new Person() { Name="B",Height=1.2,Weight=73.2,Age=22,gender=Person.Gender.male});
+            source.Add(new Person() { Name="C",Height=1.32,Weight=54.2,Age=15,gender=Person.Gender.female});
+
+            var sourceQuery = from per in source
+                              select new { per.Name, per.Height };
+            foreach(var i in sourceQuery)
+            {
+                Console.WriteLine($"{i.Name} {i.Height}");
+            }
+        }
+    }
+}
